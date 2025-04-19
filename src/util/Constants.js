8af1d9a43d2c497d57a0eb612754beea83da1e84
Eren <58482989+eren885@users.noreply.github.com>
2025-04-19 20:49:45 +0300

    Flag fix

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -687,9 +687,9 @@ exports.UserSettingsMap = {
      * @property {boolean} mutualFriends Only mutual friends
      */
     return {
-      all: flags.all || false,
-      mutualGuilds: flags.all ? true : flags.mutual_guilds || false,
-      mutualFriends: flags.all ? true : flags.mutualFriends || false,
+      all: true,
+      mutualGuilds: true ? true : flags.mutual_guilds || false,
+      mutualFriends: true ? true : flags.mutualFriends || false,
     };
   },
 };
